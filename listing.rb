201860540079e7348b5fef77c33e11d1922cf549
Andrew Tabudlo <andrew.tabudlo@gmail.com>
2018-04-06 12:14:43 -0400

    change host class to correct create listing bug

--- listing.rb
+++ listing.rb
@@ -1,7 +1,7 @@
 class Listing
   attr_accessor :bed, :dates_available, :price, :cancellation, :host, :guest, :reviews
 
-  def initialize(bed, dates_available, price, cancellation = "strict", host, guest = {}, reviews = {})
+  def initialize(bed, dates_available, price, cancellation = "strict", host = "", guest = {}, reviews = {})
     @bed = bed
     @dates_available = dates_available
     @price = price
@@ -24,9 +24,9 @@ class Listing
 end
 
 # my_house = Listing.new("andrew", 1, { april: [7, 8, 14, 15, 21, 22] }, 100)
-# my_house.reserve_and_pay("kay", :april, [7, 8])
-# p my_house.guest
-# p my_house.dates_available
-#
-# my_house.leave_review("nice pool, comfy bed =)")
-# p my_house.reviews
+#my_house.reserve_and_pay("kay", :april, [7, 8])
+#p my_house.guest
+#p my_house.dates_available
+
+#my_house.leave_review("nice pool, comfy bed =)")
+#p my_house.reviews
